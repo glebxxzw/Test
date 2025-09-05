72903de292ba7feb426001e7eb16e06e08eb7d3c
glebxxzw <glebxxzw@gmail.com>
2025-09-05 17:09:14 +0300

    [25w36d] task2.js

---
+++ assets/scripts/task2.js
@@ -0,0 +1,16 @@
+'use strict';
+
+//Задача на превышение скорости автомобиля
+
+const speedsArray = [];
+
+function getCardata() {
+    const obj = {
+        number: "", 
+        speed: 0
+    }; 
+
+    
+
+    return obj;
+}
